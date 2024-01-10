50d689c7af0e94ebf58e32c419ba81002defbef9
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2024-01-11 00:07:19 +0200

    feat: improved frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,29 @@
-document.getElementById('submit-guess').addEventListener('click', submitGuess);
+const audioPrompt = document.getElementById('audio-prompt');
+const chatOutput = document.getElementById('chat-output');
+
+const submitGuessButton = document.getElementById('submit-guess');
+submitGuessButton.addEventListener('click', submitGuess);
+
+document.getElementById('start').addEventListener('click', function() {
+    audioPrompt.play();
+    this.style.display = 'none';
+    submitGuessButton.style.display = 'block';
+    sendPrompt('startThread', null);
+});
 
 let threadId = null;
 
 function submitGuess() {
     const userInput = document.getElementById('user-input').value;
-    const chatOutput = document.getElementById('chat-output');
-    chatOutput.innerHTML += `<div>Ти предположи: ${userInput}</div>`;
-    sendPrompt('sendPrompt', userInput)
+    chatOutput.innerHTML += `<div>👤: ${userInput}</div>`;
+    sendPrompt('sendPrompt', userInput);
 }
 
 function sendPrompt(path, prompt) {
     let body = {}
     if (prompt !== null) {
-        body.thread_id = threadId
-        body.prompt = prompt
+        body.thread_id = threadId;
+        body.prompt = prompt;
     }
     fetch('http://10.211.55.3:5000/' + path, {
         method: 'POST',
@@ -28,10 +38,10 @@ function sendPrompt(path, prompt) {
             document.getElementById('object-image').src = data.new_image;
         }
         threadId = data.thread_id;
-        const audioPrompt = document.getElementById('audio-prompt');
+
         audioPrompt.src = "data:audio/mp3;base64," + data.audio;
         audioPrompt.play();
+        chatOutput.innerHTML += `<div>🔮: ${data.text}</div>`;
     })
 }
 
-sendPrompt('startThread', null)
