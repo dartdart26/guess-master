0d74667e6157c5a63d816c5ac48afb4aab8ca20d
Zlatko Zahariev <zlatko.zahariev@gmail.com>
2025-10-30 13:38:17 +0200

    fix: update openAI and add header to support already depricated AssistantsAPI

--- openai_client.py
+++ openai_client.py
@@ -3,4 +3,4 @@ from openai import OpenAI
 
 def get_client():
     with open("./conf/openai_api_key.txt", "r") as file:
-        return OpenAI(api_key=file.read())
+        return OpenAI(api_key=file.read(), default_headers={"OpenAI-Beta": "assistants=v2"})
